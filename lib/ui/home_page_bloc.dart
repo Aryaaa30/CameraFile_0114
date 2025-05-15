ee751ac7654532a2c01af82b5e0c797d8eb83b19
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-05-15 11:56:29 +0700

    Membuat home_page_bloc

---
+++ lib/ui/home_page_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:session7_mobile_sensor/bloc/camera_bloc.dart';
+import 'package:session7_mobile_sensor/bloc/camera_event.dart';
+import 'package:session7_mobile_sensor/bloc/camera_state.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
