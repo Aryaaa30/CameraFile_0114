5730d79bd9713efa69569b9490a203c587bd3a08
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-05-15 09:35:05 +0700

    Menambahkan inisialisasi Camera

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -5,9 +5,18 @@ part 'camera_event.dart';
 part 'camera_state.dart';
 
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
-  CameraBloc() : super(CameraInitial()) {
-    on<CameraEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  late final List<CameraDescription> _cameras;
+
+  CameraBloc() : super(const CameraInitial()) {
+    on<InitializeCamera>(_onInit);
+    on<SwitchCamera>(_onSwitchCamera);
+    on<ToggleFlash>(_onToggleFlash);
+    on<TakePicture>(_onTakePicture);
+    on<OnTapFocus>(_onTapFocus);
+    on<PickGallery>(_onPickGallery);
+    on<OpenCameraAndCapture>(_onOpenCamera);
+    on<DeleteImage>(_onDeleteImage);
+    on<ClearSnackBar>(_onClearSnackBar);
+    on<RequestPermissions>(_onRequestPermissions);
   }
 }
